73da84ce935d0d5467197b0f95af970eeb569917
Matt Bridges <mbridges.91@gmail.com>
2011-10-25 15:39:59 -0500

    Updated README
    Updated class variable for consistency
    
    Bug where list would throw error if no replacements were provided
    
    Removed redundancy in @@replacements instantiation
    
    @@replacements needed to be instantiated before replace could be called
    
    Updated README and fixed a few bugs.

--- lib/timezone/configure.rb
+++ lib/timezone/configure.rb
@@ -18,24 +18,24 @@ module Timezone
     end
     
     def self.begin
-      @@replacements ||= {}
       yield self
     end
     
     def self.replace(what, with = Hash.new)
+      replacements # instantiate @@replacements
       @@replacements[what] = with[:with]
     end
     
     def self.replacements
-      @@replacements
+      @@replacements ||= {}
     end
     
     def self.default_for_list
-      @@default_infos ||= nil
+      @@default_list ||= nil
     end
     
     def self.default_for_list=(*list)
-      @@default_infos = list.flatten!
+      @@default_list = list.flatten!
     end
     
     def self.order_list_by
