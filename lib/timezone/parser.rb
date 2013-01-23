51ed79a33b7f264d2defdc0f5137d0532526e190
Pan Thomakos <pan.thomakos@gmail.com>
2013-01-23 14:05:22 -0800

    Added main parser function.
    Still a lot of work be be done so that the parser can recognize
    different rule formats.

--- lib/timezone/parser.rb
+++ lib/timezone/parser.rb
@@ -1,3 +1,43 @@
 require 'timezone/parser/rule'
 require 'timezone/parser/data'
 require 'timezone/parser/entry'
+
+module Timezone
+  module Parser
+    def self.parse(file)
+      zone = false
+      entries = []
+
+      IO.readlines(file).map(&:strip).each do |line|
+        next if line =~ /^\s*#/
+
+        if line =~ /^Rule/
+          puts line
+          rule(line)
+          next
+        elsif line =~ /^Zone/
+          entries = []
+        end
+
+        if line != '' && !line.nil?
+          entries = entry(line)
+        else
+          process(entries)
+        end
+      end
+
+      parse(entries)
+    end
+
+    def self.process(entries)
+      return [] if entries.empty?
+
+      rules = []
+      previous = nil
+
+      entries.map{ |entry|
+        previous = entry.data(previous ? previous.last.end_date : nil)
+      }.flatten
+    end
+  end
+end
