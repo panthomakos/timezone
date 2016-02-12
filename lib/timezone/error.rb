b9563a8667776cd6ca4c014fa03adfcb7c349015
Pan Thomakos <pan.thomakos@gmail.com>
2016-02-12 10:56:31 -0800

    [0.99] Add Timezone::Error documentation

--- lib/timezone/error.rb
+++ lib/timezone/error.rb
@@ -1,5 +1,6 @@
 module Timezone
-  # Error messages that can be raised by this gem. To catch any related error message, simply use Error::Base.
+  # Error messages that can be raised by this gem. To catch any
+  # related error message, use Error::Base.
   #
   #   begin
   #     ...
@@ -7,13 +8,21 @@ module Timezone
   #     puts "Timezone Error: #{e.message}"
   #   end
   module Error
+    # Top-level error. All other timezone errors subclass this one.
     class Base < StandardError; end
+    # Indicates an invalid timezone name.
     class InvalidZone < Base; end
+    # @deprecated this class will be removed in the next release of the gem.
     class NilZone < Base; end
+    # Indicates an error during lookup using the geonames API.
     class GeoNames < Base; end
+    # Indicates an error during lookup using the google API.
     class Google < Base; end
+    # @deprecated this class will be removed in the next release of the gem.
     class ParseTime < Base; end
+    # Indicates a missing stub during a test lookup.
     class Test < Base ; end
+    # Indicates an invalid configuration.
     class InvalidConfig < Base ; end
   end
 end
